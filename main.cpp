fa014186ce6691ddfdac2152ce11cd1b43fdd3d3
Reed0610 <shanereed19@gmail.com>
2023-08-13 16:08:23 +0000

    Final Code
    Signed-off-by: Reed0610 <shanereed19@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+// Initialize DigitalIn and DigitalOut pins
+DigitalIn button1(PB_13);
+DigitalIn button2(PB_16);
+DigitalOut led1(PC_7);
+DigitalOut led2(PC_9);
 
+int main() {
+    while (1) {
+        if (button1) {
+            // Button on p13 is pressed
+            led1 = 1;  // Turn on p23
+            led2 = 0;  // Turn off p25
+        } else if (button2) {
+            // Button on p16 is pressed
+            led1 = 0;  // Turn off p23
+            led2 = 1;  // Turn on p25
+        } else {
+            // Neither button is pressed
+            led1 = 0;  // Turn off p23
+            led2 = 0;  // Turn off p25
+        }
     }
 }
-
